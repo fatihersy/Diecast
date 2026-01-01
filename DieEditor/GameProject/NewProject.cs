7ca2c7dfea3aa205b38d5174ce3a240a701dac15
fatihersy <fatih.r.ersoy@gmail.com>
2026-01-01 21:04:20 +0300

    Video 2

--- DieEditor/GameProject/NewProject.cs
+++ DieEditor/GameProject/NewProject.cs
@@ -1,39 +1,97 @@
-﻿using System;
+﻿using DieEditor.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DieEditor.GameProject
 {
-
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+		[DataMember]
+		public byte[] Icon { get; set; }
+		[DataMember]
+		public byte[] Screenshot { get; set; }
+		[DataMember]
+        public string IconFilePath { get; set; }
+		[DataMember]
+		public string ScreenshotFilePath { get; set; }
+		[DataMember]
+		public string ProjectFilePath { get; set; }
+	}
 
     class NewProject : ViewModelBase
-    {
-        private string _name = " % Name % ";
-        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\DieProjects\";
-        public string Name         {
-            get { return _name; }
+	{
+		// TODO: get the path from the installation location
+		private readonly string _templatePath = @"..\..\DieEditor\ProjectTemplates\";
+
+		private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+
+        private string _projectName = " % Name % ";
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\DieProjects\";
+        public string ProjectName         {
+            get { return _projectName; }
             set
             {
-                if (_name != value)
+                if (_projectName != value)
                 {
-                    _name = value;
-                    OnPropertyChanged(nameof(Name));
+					_projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
                 }
             }
         }
-        public string Path
+        public string ProjectPath
         {
-            get { return _path; }
+            get { return _projectPath; }
             set
             {
-                if (_path != value)
+                if (_projectPath != value)
                 {
-                    _path = value;
-                    OnPropertyChanged(nameof(Path));
+					_projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
                 }
+            }
+        }
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var file in templateFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    
+                    template.IconFilePath       = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+					
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "ss.png"));
+					template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+					
+                    _projectTemplates.Add(template);
+                }
+            } 
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                // TODO: Log
             }
         }
     }
---
+++ DieEditor/Utilities/Serializer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieEditor.Utilities
+{
+    public static class Serializer
+    {
+        public static void toFile<T>(T instance, string filePath)
+        {
+            try
+            {
+                using (var writer = new System.IO.StreamWriter(filePath))
+                {
+                    var serializer = new DataContractSerializer(typeof(T));
+                    serializer.WriteObject(writer.BaseStream, instance);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error serializing to file: " + ex.Message);
+                // TODO: Log
+            }
+        }
+
+        internal static T FromFile<T>(string path)
+        {
+            try
+            {
+                using (var reader = new System.IO.StreamReader(path, true))
+                {
+                    var serializer = new DataContractSerializer(typeof(T));
+                    T instance = (T) serializer.ReadObject(reader.BaseStream);
+                    return instance;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error serializing to file: " + ex.Message);
+                // TODO: Log
+                return default(T);
+            }
+        }
+    }
+}
